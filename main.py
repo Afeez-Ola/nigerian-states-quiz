c07ee29ab15103642f476c05c14d6188675f44e6
Afeez Bolaji <afeezbolajiola@gmail.com>
2023-04-25 13:41:00 +0200

    Finished the game.

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-import pandas
 import turtle
+import pandas
+from scoring import Score
+
+# Initialize Score class
+scoreboard = Score()
+score = scoreboard.score_board()
 
 # Set up screen
 screen = turtle.Screen()
-screen.title("U.S States Game")
-screen.setup(width=900, height=900)
+screen.title("Nigeria State Game")
+screen.setup(width=840, height=780)
 screen.addshape("nigeria40.gif")
 turtle.shape("nigeria40.gif")
 
 # Load states data from CSV
-states_dict = pandas.read_csv("50_states.csv").to_dict()
+states_dict = pandas.read_csv("states.csv").to_dict()
 states = states_dict["state"]
 x_coor = states_dict["x"]
 y_coor = states_dict["y"]
@@ -22,5 +27,26 @@ attempts = 50
 guessed_state = []
 missing_states = []
 
-
-screen.exitonclick()
+# Game loop
+while len(guessed_state) < attempts:
+    answer_text = (screen.textinput(title=score, prompt="What's another state's name? ")).title()
+    if answer_text == "Exit":
+        # Check for missing states
+        for item in state_list:
+            if item not in guessed_state:
+                missing_states.append(item)
+        remaining_states = pandas.DataFrame({"state": missing_states})
+        remaining_states.to_csv("remaining_states.csv")
+        break
+    if answer_text in state_list:
+        # Update turtle with guessed state name
+        text_turtle = turtle.Turtle()
+        text_turtle.hideturtle()
+        text_turtle.penup()
+        index = state_list.index(answer_text)
+        text_turtle.goto(x_list[index], y_list[index])
+        text_turtle.write(answer_text)
+        guessed_state.append(answer_text)
+        score = scoreboard.update_score()
+    else:
+        print("Nope: " + answer_text)
---
+++ scoring.py
@@ -0,0 +1,15 @@
+from turtle import Turtle
+
+
+class Score(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.result = "0/36 states correct"
+        self.color("black")
+
+    def update_score(self):
+        self.result = f"{int(self.result.split('/')[0]) + 1}/36 states correct"
+        return self.result
+
+    def score_board(self):
+        return self.result
